aeccadb830f58c5b3a2780650ac08b4e72ff5107
Moranpx1 <Moranpx1@gmail.com>
2023-06-19 22:50:06 +0300

    Adding login handlers

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,14 +1,34 @@
 import Rect, {useState} from 'react';
 
 const Login = () => {
+
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
+  }
+
+  
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
+  //Censor password
+  const censoredPassword = '*'.repeat(password.length);
+
+  const handleSubmit = (e) => {
+    e.preventDafault();
+  }
+
     return (
         <>
           <form>
             <label form = "user_name">User name</label>
-            <input type = "text" placeholder="Enter user name" id = "user_name" name = "user_name"></input>
+            <input type = "text" value = {userName} placeholder="Enter user name" id = "userName" name = "userName" onChange = {handleUserNameChange}></input>
             <label form = "password">Password</label>
-            <input type = "text" placeholder="Enter password" id = "password" name = "password"></input>
-            <button>Log In</button>
+            <input type = "text" value = {censoredPassword} placeholder="Enter password" id = "password" name = "password" onChange = {handlePasswordChange}></input>
+            <button type = "submit">Log In</button>
           </form>
           <button>Don't have an account? Click to register</button>
         </>
